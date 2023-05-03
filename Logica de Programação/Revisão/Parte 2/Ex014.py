76e18b7dfc3c592d0af9cec388d3f0e38c3899fb
Tiago Rôxo <83374845+roxootiago@users.noreply.github.com>
2023-05-03 16:58:39 -0300

    eita como estuda

---
+++ Logica de Programação/Revisão/Parte 2/Ex014.py
@@ -0,0 +1,11 @@
+# Escreva um algoritmo para ler dois valores (
+# considere que não serão lidos valores iguais)
+# e escrevê-los em ordem crescente
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+if num1 > num2:
+    print(num2,num1)
+else:
+    print(num1,num2)
