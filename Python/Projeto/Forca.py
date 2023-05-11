cea95038925a4ea22a7bc78521c97c0983b4659f
Tiago Rôxo <83374845+roxootiago@users.noreply.github.com>
2023-05-11 00:19:59 -0300

    forcav1

--- Python/Projeto/Forca.py
+++ Python/Projeto/Forca.py
@@ -1,18 +1,48 @@
 from random import choice
-champs = ["YASUO","KATARINA","JINX","WUKONG","ZED"]
 
-print("---x--- JOGO DA FORCA ---x---")
-print("--- ADVINHE O NOME DO CHAMP ---")
+def game():
+    champs = ["KATARINA","AMUMU", "ZED"]
+    lista = []
 
+    print("---x--- JOGO DA FORCA ---x---")
+    print("--- ADVINHE O NOME DO CHAMP ---")
 
-champsRand = choice(champs)
-#letras = len(champsRand)
+    champsRand = choice(champs)
 
-count = 0
-for i in champsRand:
-    count += 1
+    # letras = len(champsRand)
+    def tentativa():
+        tentar = input("\nVocê já sabe a resposta? ").upper()
 
-print(f"O campeão tem {count} letras")
-print(f" _ "*count)
+        if tentar == "S":
+            champTentativa = input("Digite sua tentativa: ").upper()
+            if champTentativa == champsRand:
+                print(f"Parabéns! Você acertou!\nSeu chute: {champTentativa}")
+            else:
+                print("Você errou! Tente novamente")
+                tentativa()
+        else:
+            print("Ok")
 
-letra = input("Digite uma letra: ")
+    count = 0
+    for i in champsRand:
+        count += 1
+
+    print(f"O campeão tem {count} letras")
+    for k in range(count):
+        lista.append("_")
+
+    for y in range(count):
+        print(lista[y], end=" ")
+    print()
+
+    letra = input("Digite uma letra: ").upper()
+
+    for z in range(count):
+        if champsRand[z] == letra:
+            lista[z] = letra
+
+    for p in range(count):
+        print(lista[p], end=" ")
+    tentativa()
+
+game()
