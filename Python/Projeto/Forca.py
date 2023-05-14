7a6cbafcd72ff28cc3348280010fd94c288ded2a
Tiago Rôxo <83374845+roxootiago@users.noreply.github.com>
2023-05-13 23:31:44 -0300

    adicionando algumas funcionalidades
    adicionei a funcionalidade de caso o campeão possua em seu nome espaços ou apóstrofo (') substitua automaticamente

--- Python/Projeto/Forca.py
+++ Python/Projeto/Forca.py
@@ -10,52 +10,57 @@ def Forca():
             print("Encerrando jogo...")
 
     def tentativa():
-        tentar = input("\nVocê já sabe a resposta? ").upper()
-
-        if tentar == "S" or tentar == "SIM":
-            champTentativa = input("Digite sua tentativa: ").upper()
-            if champTentativa == champsRand:
-                print(
-                    f"Parabéns! Você acertou!\nCampeão selecionado: {champsRand}: {titleChamps().capitalize()}")
-                continuar()
-            else:
-                print(champTentativa)
-                print("Você errou! Tente novamente")
-                Forca()
+        champTentativa = input("Digite sua tentativa: ").upper()
+        if champTentativa == champsRand:
+            print(
+                f"Parabéns! Você acertou!\nCampeão selecionado: {champsRand}: {titleChamps().capitalize()}")
+            opcao = input("Deseja jogar novamente").upper()
         else:
+            print(champTentativa)
+            print("Você errou! Tente novamente")
             jogar()
             tentativa()
-
-    champsRand = nameChampion().upper()
-    emptyList = []
-    count = 0
 
     print(" ---x--- JOGO DA FORCA ---x---")
     print("--- ADIVINHE O NOME DO CHAMP ---")
 
-    for i in champsRand:
-        count += 1
-    print(champsRand)
-    print(f"\nDica: {titleChamps().capitalize()}\nO campeão tem {count} letras")
-    for k in range(count):
-        emptyList.append("_")
+    champsRand = nameChampion().upper()
+    emptyList = []
 
-    for y in range(count):
-        print(emptyList[y], end=" ")
-    print()
+    print(champsRand)
+    print(f"\nDica: {titleChamps().capitalize()}\nO campeão tem {len(champsRand)} letras")
 
     def jogar():
-        kick = input("Digite uma letra: ").upper()
 
-        for z in range(count):
-            if champsRand[z] == kick:
-                emptyList[z] = kick
+        for k in range(len(champsRand)):
+            emptyList.append("_")
 
-        for p in range(count):
-            print(emptyList[p], end=" ")
+        for t in range(len(champsRand)):
+            if "'" in champsRand[t]:
+                emptyList[t] = "'"
+            if " " in champsRand[t]:
+                emptyList[t] = " "
 
-    jogar()
-    tentativa()
+        for y in range(len(champsRand)):
+            print(emptyList[y], end=" ")
+        print()
+
+        kick = input("Digite uma letra ou 'tentar' para tentar a palavra: ").upper()
+
+        if kick == "TENTAR":
+            tentativa()
+        elif kick != "TENTAR":
+            for z in range(len(champsRand)):
+                if champsRand[z] == kick:
+                    emptyList[z] = kick
+                elif kick not in champsRand:
+                    print("esta letra não está presente")
+                    break
+            for p in range(len(champsRand)):
+                print(emptyList[p], end=" ")
+            jogar()
+
+    return jogar()
 
 
 Forca()
